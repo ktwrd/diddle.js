c8b8ef132a1daeb27f21ba3d7bb828ac753e5745
Jyles Coad-Ward <jyles@dariox.club>
2021-07-27 19:43:28 +0800

    Moved Script Detection to engine/scriptman.js
    Fixed Debug/Development Mode Detection

--- cli.js
+++ cli.js
@@ -8,6 +8,7 @@ module.exports = {
 }
 
 const fs = require("fs");
+const path = require("path");
 
 var LaunchParameters = null;
 
@@ -35,7 +36,7 @@ var LaunchParameters = null;
 	LaunchParameters = argvParsed;
 } )();
 
-var CustomConfigData = {};
+var CustomConfigData = LaunchParameters;
 
 const dlog = {
 	w: (...c) => {
@@ -95,139 +96,16 @@ if (LaunchParameters.config != undefined) {
 	}
 }
 
-// Fetch User Scripts
-( () => {
-	var timestamp_start = Date.now();
+CustomConfigData.scripts_directory = CustomConfigData.scripts_directory || "./scripts/";
 
-	dlog.i(`fetching scripts...`);
+if (CustomConfigData.scripts_directory.startsWith(".")) {
+	CustomConfigData.scripts_directory = path.join(__dirname,CustomConfigData.scripts_directory)
+} else if (CustomConfigData.scripts_directory.startsWith(/[a-zA-Z0-9]{0,2}/)) {
+	CustomConfigData.scripts_directory = path.join(__dirname,CustomConfigData.scripts_directory);
+} else {
+	CustomConfigData.scripts_directory = path.resolve(CustomConfigData.scripts_directory);
+}
 
-	var scriptsdirectory = LaunchParameters["script-directory"] || CustomConfigData.scripts_directory || "./scripts/";
+var DoDebug = LaunchParameters.debug || LaunchParameters.developer || CustomConfigData.developer || CustomConfigData.debug || false;
 
-	if (!fs.existsSync(scriptsdirectory)) {
-		try {
-			fs.mkdirSync(scriptsdirectory)
-		} catch(e) {
-			dlog.e(`cannot create scripts directory\n`,e);
-			process.exit(1);
-		}
-		dlog.i(`created scripts directory`);
-	}
-
-	// Get Script Files
-	var scripts_unfiltered,scripts_filenames,scripts_filtered = [];
-
-	try {
-		scripts_filenames = fs.readdirSync(scriptsdirectory).filter(f => f.endsWith(".js"));
-	} catch(e) {
-		dlog.e(`cannot fetch scripts directory\n`,e);
-		process.exit(1);
-	}
-
-	var ScriptScheme = {
-		manifest: {
-			author: 'r',
-			license: 'r',
-			source: 'o',
-			type: 'r'
-		},
-		event: {},
-	}
-	for ( let j = 0; j < scripts_filenames.length; j++ ) {
-		try {
-			let currentscript = require(`${scriptsdirectory}${scripts_filenames[j]}`);
-
-			let currentscript_fname = scripts_filenames[j];
-
-			var ValidScript = { };
-
-			var doSkipScript = false;
-			var skipReason = null;
-
-			// Check Event Object
-			if (currentscript.event != undefined) {
-				ValidScript.event = {};
-				var eventobjs = Object.entries(currentscript.event)
-				for (let i = 0; i < eventobjs.length; i++) {
-					if (typeof eventobjs[i][1] == "function") {
-						ValidScript.event[eventobjs[i][0]] = eventobjs[i][1];
-					} else {
-						skipReason = `event '${eventobjs[i][0]}' for script '${scripts_filenames[j]}' has typeof '${typeof eventobjs[i][1]}'. expected typeof 'function'. skipping script...`
-						dlog.e(skipReason);
-						doSkipScript = true;
-					}
-				}
-			} else {
-				dlog.w(`no events found for script '${scripts_filenames[j]}'`);
-			}
-
-			// Check Script Manifest
-			if (currentscript.manifest != undefined && doSkipScript == false) {
-				ValidScript.manifest = {};
-				var manifestobjs = Object.entries(ScriptScheme.manifest);
-
-				for (let i = 0; i < manifestobjs.length; i++) {
-					var c_manifestobj = manifestobjs[i];
-
-					// Check if Manifest Entry Exists
-					if (currentscript.manifest[c_manifestobj[0]] == undefined) {
-						switch (c_manifestobj[1]) {
-							case 'r':
-							case 1:
-								doSkipScript = true;
-								skipReason = `missing required script manifest entry '${c_manifestobj[0]}' for script '${currentscript_fname}'. skipping...`
-								dlog.e(skipReason);
-								break;
-							case 'o':
-							case 0:
-								dlog.w(`missing optional script manifest entry '${c_manifestobj[0]}' for script '${currentscript_fname}'`);
-								break;
-						}
-					} 
-					// Check Manifest Entry Type
-					else if (typeof currentscript.manifest[c_manifestobj[0]] != typeof c_manifestobj[1]) {
-						switch (c_manifestobj[1]) {
-							case 'r':
-								doSkipScript = true;
-								skipReason = `invalid type manifest entry '${c_manifestobj[0]}' for script '${currentscript_fname}'. skipping...`;
-								dlog.e(skipReason);
-								break;
-							case 'o':
-								dlog.w(`invalid type manifest entry '${c_manifestobj[0]}' for script '${currentscript_fname}'`);
-								break;
-						}
-					} 
-					// Manifest Entry Exists and is matching type of scheme
-					else {
-						dlog.d(`valid manifest entry '${c_manifestobj[0]}' for script '${currentscript_fname}'`);
-						ValidScript.manifest[c_manifestobj[0]] = currentscript.manifest[c_manifestobj[0]];
-					}
-				}
-
-				if (currentscript.manifest.type == undefined && doSkipScript == false) {
-					dlog.w(`manifest type is undefined, assuming as 'library'`);
-					ValidScript.manifest.type = ValidScript.manifest.type || 'library';
-				}
-			} else if (currentscript.manifest == undefined) {
-				skipReason = `manifest for script '${currentscript_fname}' is undefined`;
-				doSkipScript = true;
-				dlog.e(skipReason);
-			}
-
-			if (doSkipScript == false) {
-				scripts_filtered.push(ValidScript);
-			}
-		} catch(e) {
-			dlog.e(`cannot get script\n`,e);
-			process.exit(1);
-		}
-	}
-
-	if (scripts_filtered.length < 1) {
-		dlog.e(`no valid scripts found, aborting!`);
-		process.exit(1);
-	} else {
-		dlog.d(`${scripts_filtered.length} script${scripts_filtered.length == 1 ? "":"s"} found. took ${Date.now() - timestamp_start}ms`);
-	}
-} )();
-
-const diddleInstance = new (require("."))(CustomConfigData);
+const diddleInstance = new (require("."))(CustomConfigData, DoDebug);
