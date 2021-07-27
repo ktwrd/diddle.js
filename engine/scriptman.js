09f9845673da320e112e16f4d13b9cdae17943fb
Jyles Coad-Ward <jyles@dariox.club>
2021-07-27 14:31:30 +0800

    Added Manifest and Logger

--- engine/scriptman.js
+++ engine/scriptman.js
@@ -1,4 +1,13 @@
+const Logger = require("./logger")
+
 class ScriptManager {
+
+	manifest = {
+		version: '0.1b',
+		name: 'diddle.js/scriptman'
+	}
+
+	log = new Logger(this.diddle,this.manifest.name);
 	constructor(diddle) {
 		this.diddle = diddle;
 	}
