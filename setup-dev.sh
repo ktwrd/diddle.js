937cace356315980ffab0c2b669cbcd2a5a9e593
Jyles Coad-Ward <jyles@dariox.club>
2021-07-28 13:33:46 +0800

    I think I fixed it :/

--- setup-dev.sh
+++ setup-dev.sh
@@ -12,25 +12,29 @@ echo "% git clone $REPO -b $BRANCH_DOCS $BRANCH_DOCS"
 git clone $REPO -b $BRANCH_DOCS $BRANCH_DOCS
 echo "% cd dev"
 cd dev
-echo "\n# [dev]  Installing Modules"
+echo "# [dev]  Installing Modules"
 echo "% npm i --include=dev"
 npm i --include=dev
 
-echo "\n% npm link"
+echo "% npm link"
 if npm link; then
 	echo "Linked 'diddle.js' as local module"
 else
 	echo "Need superuser permissions to link module"
-	echo "\n% sudo npm link"
+	echo "% sudo npm link"
 	sudo npm link
 fi
 
 echo "% cd .. && cd docs"
 cd .. && cd docs
-echo "\n# [docs] Installing Modules"
+echo "# [docs] Installing Modules"
 echo "% npm i --include=dev"
 npm i --include=dev
 
-#echo "\n# [docs] Installing git submodules"
+echo "# [docs] Installing git submodules"
+echo "% git submodule add source/"
+git submodule add source/
+echo "% git submodule add source-dev/"
+git submodule add source-dev/
 #git submodule add -b $BRANCH_DEV -f $REPO.git source-dev
 #git submodule add -b $BRANCH_STABLE -f $REPO.git source
